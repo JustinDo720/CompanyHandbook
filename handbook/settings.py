ebc196cc58f489c643b0f417f57726700ea09d03
Justin Do <56330379+JustinDo720@users.noreply.github.com>
2025-09-09 17:25:07 -0400

    Integrated Pinecone + Langchain to our POST method. Split, Embed + Injest

--- handbook/settings.py
+++ handbook/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+# Loading up our .env file 
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -28,6 +31,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Pinecone Keys
+PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+PINECONE_INDEX = os.getenv('PINECONE_INDEX')
 
 # Application definition
 
